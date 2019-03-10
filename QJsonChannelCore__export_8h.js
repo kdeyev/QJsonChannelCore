03ec1cc9089e4f808fd29cb792ee6a31a9f51a54
kdeyev <k.deyev@gmail.com>
2019-03-10 22:01:33 +0200

    Documentation

---
+++ QJsonChannelCore__export_8h.js
@@ -0,0 +1,8 @@
+var QJsonChannelCore__export_8h =
+[
+    [ "QJSONCHANNELCORE_EXPORT", "QJsonChannelCore__export_8h.html#a19489cf1949bc2437803a045ed8f4abf", null ],
+    [ "QJSONCHANNELCORE_NO_EXPORT", "QJsonChannelCore__export_8h.html#a46072a202dabf7c304ab4f96abed38b2", null ],
+    [ "QJSONCHANNELCORE_DEPRECATED", "QJsonChannelCore__export_8h.html#a6989007a74f0f2037f64f1552e6783a7", null ],
+    [ "QJSONCHANNELCORE_DEPRECATED_EXPORT", "QJsonChannelCore__export_8h.html#a534c3a6b336f14bf992fef4242b5caf1", null ],
+    [ "QJSONCHANNELCORE_DEPRECATED_NO_EXPORT", "QJsonChannelCore__export_8h.html#af51efbd6366f7b817f069bb43f1e6e67", null ]
+];
---
+++ search/defines_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['qjsonchannelcore_5fdeprecated',['QJSONCHANNELCORE_DEPRECATED',['../QJsonChannelCore__export_8h.html#a6989007a74f0f2037f64f1552e6783a7',1,'QJsonChannelCore_export.h']]],
+  ['qjsonchannelcore_5fdeprecated_5fexport',['QJSONCHANNELCORE_DEPRECATED_EXPORT',['../QJsonChannelCore__export_8h.html#a534c3a6b336f14bf992fef4242b5caf1',1,'QJsonChannelCore_export.h']]],
+  ['qjsonchannelcore_5fdeprecated_5fno_5fexport',['QJSONCHANNELCORE_DEPRECATED_NO_EXPORT',['../QJsonChannelCore__export_8h.html#af51efbd6366f7b817f069bb43f1e6e67',1,'QJsonChannelCore_export.h']]],
+  ['qjsonchannelcore_5fexport',['QJSONCHANNELCORE_EXPORT',['../QJsonChannelCore__export_8h.html#a19489cf1949bc2437803a045ed8f4abf',1,'QJsonChannelCore_export.h']]],
+  ['qjsonchannelcore_5fno_5fexport',['QJSONCHANNELCORE_NO_EXPORT',['../QJsonChannelCore__export_8h.html#a46072a202dabf7c304ab4f96abed38b2',1,'QJsonChannelCore_export.h']]],
+  ['qjsonchanneldebug',['QJsonChannelDebug',['../QJsonChannelMessage_8h.html#a619ab86ca360b9464dc901ef38e20678',1,'QJsonChannelGlobal.h']]]
+];
